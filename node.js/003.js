8651836088ef8550021f98e720b3c24a5f54b15c
Dan <woodson.dan@gmail.com>
2013-10-22 22:34:00 -0500

    WIP

--- node.js/003.js
+++ node.js/003.js
@@ -2,30 +2,29 @@
  * Problem 3
  * Find the largest prime factor of a composite number.
 */
-var sol = function(comp){
-  var ret = Math.floor(comp/2),
-      primeTest;
-
-  primeTest = function(num){
-    var count = 2,
-        half = Math.floor(num/2),
-        isPrime = true;
-    while(count < half){
-      if(num % count === 0){
-        isPrime = false;
-      }
-      count++;
+var primeTest = function(num){
+  var count   = 2,
+      half    = Math.floor(num/2),
+      isPrime = true;
+  while(count < half){
+    if(num % count === 0){
+      isPrime = false;
+      break;
     }
-    return isPrime;
-  };
+    count++;
+  }
+  return isPrime;
+};
 
-  while(ret){
+var sol = function(comp){
+  var ret = Math.ceil(comp/2);
+  while(ret > 1){
+    console.log('testing: '+ret);
     if(comp % ret === 0 && primeTest(ret)){
       break;
     }
     ret--;
   }
-
   return ret;
 };
 
